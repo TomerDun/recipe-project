a7216f32e5de9ea933411710dfc864ae64b94f31
TomerDun <tomer@dunsky.name>
2025-08-28 17:06:52 +0300

    Added morgan logging

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 import express from 'express'
 import { recipeRouter } from './routes/recipeRouter.js';
 import { errorHandler } from './middleware/errorHandler.js';
+import morgan from 'morgan';
 
 const app = express();
 
 // Middleware
 app.use(express.json());
+app.use(morgan('dev'))
 
 // Routes
 app.use('/recipes', recipeRouter);
