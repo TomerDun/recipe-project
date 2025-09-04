fd450af535e661d8e89e7ca5bf0e909df95bd632
TomerDun <tomer@dunsky.name>
2025-09-04 16:27:52 +0300

    Added login function

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,11 +1,21 @@
-import { createUser } from "../db/models/authModel.js";
+import { createUser, getUser } from "../db/models/authModel.js";
 import jwt from 'jsonwebtoken';
+
+
+// --Utils--
+
+function generateToken(data) {
+    const newToken = jwt.sign(data, process.env.JWT_SECRET, {expiresIn: '1h'});
+    return newToken;
+}
+
+// --API Handlers--
 
 // TODO: Add tokens to cookies
 export async function register(req, res) {
     const newUser = await createUser(req.body);
 
-    const newToken = generateToken({userId: newUser.id, email: newUser.email});
+    const newToken = generateToken({id: newUser.id, email: newUser.email});
     res.status(201).json({
         'message': 'created new user',
         user: newUser,
@@ -13,7 +23,10 @@ export async function register(req, res) {
     })
 }
 
-function generateToken(data) {
-    const newToken = jwt.sign(data, process.env.JWT_SECRET, {expiresIn: '1h'});
-    return newToken;
+export async function login(req, res) {
+    const user = await getUser(req.body.email, req.body.password);
+    // Generate token for user
+    const token = generateToken({id: user.id, email: user.email});
+    res.status(200).json({user, token});
 }
+
--- db/models/authModel.js
+++ db/models/authModel.js
@@ -1,31 +1,33 @@
 import { v4 } from "uuid";
-import {sequelize} from '../connection.js'
+import { sequelize } from '../connection.js'
 import bcrypt from 'bcrypt';
 
 
-function hashPass(password){
+function hashPass(password) {
     const saltRounds = 10;
     const salt = bcrypt.genSaltSync(saltRounds);
-    return bcrypt.hashSync(password,salt);
+    return bcrypt.hashSync(password, salt);
 }
 
 // TODO: Add password hashing!
-export async function createUser({username, password, email, firstName, lastName}) {    
+export async function createUser({ username, password, email, firstName, lastName }) {
     const id = v4();
     const query = `INSERT INTO users (username, password, email, firstName, lastName)
     VALUES (:username, :password, :email, :firstName, :lastName)`;
     // const password = encryptPassword(password);
     password = hashPass(password);
 
-    const [res] = await sequelize.query(query, 
-        {replacements: {
-            id: id,
-            username: username,
-            password: password,
-            email: email,
-            firstName: firstName,
-            lastName: lastName        
-        }});
+    const [res] = await sequelize.query(query,
+        {
+            replacements: {
+                id: id,
+                username: username,
+                password: password,
+                email: email,
+                firstName: firstName,
+                lastName: lastName
+            }
+        });
     console.log('➕ Added new user to DB');
 
     return {
@@ -34,5 +36,40 @@ export async function createUser({username, password, email, firstName, lastName
         email: email,
         firstName: firstName,
         lastName: lastName
-    }     
+    }
+}
+
+export async function getUser(email, password) {
+    const [res] = await sequelize.query(`SELECT * FROM users WHERE email = :email`, { replacements: { email } });
+
+    // User not found
+    if (!res.length) {
+        console.error('getUser: email not found')
+        const err = new Error('Invalid email/password')
+        err.status = 403;
+        throw err;
+    }
+
+    const user = res[0];
+    console.log('user: ', user);
+    
+    const passwordValid = await bcrypt.compare(password, user.password);
+    console.log('valid: ', passwordValid);
+    
+    if (!passwordValid) {
+        console.error('getUser: password invalid')
+        const err = new Error('Invalid email/password')
+        err.status = 403;
+        throw err;
+    }
+
+    return {
+        id: user.id,
+        username: user.username,
+        email: user.email,
+        firstName: user.firstName,
+        lastName: user.lastName
+    }
+
+
 }
