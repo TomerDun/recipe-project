6069061260977f6df063dc5e13207cc5d60a7a13
TomerDun <tomer@dunsky.name>
2025-09-04 16:06:08 +0300

    Added JWT token to register function

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,9 +1,19 @@
 import { createUser } from "../db/models/authModel.js";
+import jwt from 'jsonwebtoken';
 
+// TODO: Add tokens to cookies
 export async function register(req, res) {
     const newUser = await createUser(req.body);
+
+    const newToken = generateToken({userId: newUser.id, email: newUser.email});
     res.status(201).json({
         'message': 'created new user',
-        user: newUser
+        user: newUser,
+        token: newToken,
     })
+}
+
+function generateToken(data) {
+    const newToken = jwt.sign(data, process.env.JWT_SECRET, {expiresIn: '1h'});
+    return newToken;
 }
